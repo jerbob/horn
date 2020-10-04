c75579a030709a195c898e161a2dfc377a3456bb
Jeremiah Boby <mail@jeremiahboby.me>
2020-10-04 02:43:32 +0100

    Move static JSON files to data/

--- huntinghorn/constants.py
+++ huntinghorn/constants.py
@@ -6,16 +6,16 @@ from typing import Dict, List
 
 from huntinghorn.utils import Horn, Melody, Move, Note
 
-ROOT = Path(__file__).parent
+DATA_ROOT = Path(__file__).parent / "data"
 
 
-with (ROOT / "horns.json").open() as file:
+with (DATA_ROOT / "horns.json").open() as file:
     horns: Dict[str, Horn] = {
         name: Horn(name, moves=[Move(move[0], Note(move[1])) for move in horn])
         for name, horn in json.load(file).items()
     }
 
-with (ROOT / "melodies.json").open() as file:
+with (DATA_ROOT / "melodies.json").open() as file:
     melodies: List[Melody] = [
         Melody(name=melody[0], actions=[Note(note) for note in melody[1]])
         for melody in json.load(file)
